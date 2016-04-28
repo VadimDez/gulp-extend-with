a38a090741a778696ddba89aabf8fa24ae15ec6f
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-04-29 00:08:26 +0200

    further init

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 /**
  * Created by Vadym Yatsyuk on 28/04/16
  */
+
+const path = require('path');
+const fs = require('fs');
+
+module.exports = function () {
+
+};
---
+++ test/tests.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vadym Yatsyuk on 29/04/16
+ */
+
+"use strict";
+
+let mocha = require('mocha');
+let should = require('should');
+
+describe('gulp-extend-with', () => {
+
+});
